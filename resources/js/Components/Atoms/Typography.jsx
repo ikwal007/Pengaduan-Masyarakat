e8b4637c2309216cd430cda33a0d49f51edf3072
Muhammad Ikwal Ramadhan <muhammadikwalr@gmail.com>
2024-03-21 12:46:34 +0700

    style: Apply theme to Typography component for harmonious color in speci fic cases
    Melakukan penerapan tema ke komponen Typography agar warnanya terlihat
    harmonis dalam kasus-kasus tertentu.

--- resources/js/Components/Atoms/Typography.jsx
+++ resources/js/Components/Atoms/Typography.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Typography = ({tag = 'h1', children, className}) => {
+const Typography = ({tag = 'h1', children, className, theme = "secondary"}) => {
+    const themeColors = {
+        primary: {
+            text: "text-white",
+        },
+        secondary: {
+            text: "text-gray-700"
+        }
+    }[theme];
     const Tag = tag
     return (
-        <Tag className={`${className} text-gray-700 dark:text-gray-200 dark:group-hover:text-gray-700 transition duration-700 ease-in-out`}>
+        <Tag className={`${className} ${themeColors.text} dark:text-gray-200 dark:group-hover:text-gray-700 transition duration-700 ease-in-out`}>
             {children}
         </Tag>
     );
