73669fad29061b5d5b0a55b4a8bb4d610ab83aeb
Muhammad Ikwal Ramadhan <muhammadikwalr@gmail.com>
2024-03-21 13:50:54 +0700

    style: Implement typography theme in Desktop component
    Melakukan implementasi tema tipografi dalam komponen Desktop untuk menyesuaikan perubahan pada komponen Typography.

--- resources/js/Components/Sidebars/Desktop.jsx
+++ resources/js/Components/Sidebars/Desktop.jsx
@@ -206,10 +206,10 @@ const Desktop = () => {
                                 height="sm"
                                 className="h-max"
                             >
-                                <Typography tag="span" className="ml-4">
+                                <Typography tag="span" className="ml-2" theme="primary">
                                     Create User Account
                                 </Typography>
-                                <Typography tag="span">
+                                <Typography tag="span" theme="primary">
                                     <MdAdd className="w-5 h-5" />
                                 </Typography>
                             </GlobalLink>
