c51c59349a3ddf72207e210732e08c0991559bfc
Muhammad Ikwal Ramadhan <muhammadikwalr@gmail.com>
2024-03-26 23:36:46 +0700

    Refactor data rendering method in DashboardManagesWorkerAccounts/Index.jsx
    This commit introduces a refactor to the data rendering approach in the
    Index component of DashboardManagesWorkerAccounts. The previous method
    was replaced with a more streamlined and efficient process for displaying
    data in the table. This enhancement aims to improve code readability and
    maintainability.

--- resources/js/Pages/SuperAdmin/DashboardManagesWorkerAccounts/Index.jsx
+++ resources/js/Pages/SuperAdmin/DashboardManagesWorkerAccounts/Index.jsx
@@ -3,7 +3,7 @@ import AuthenticatedLayout2 from "@/Layouts/AuthenticatedLayout2";
 // import { useEcho } from "@/utils/EchoContext";
 import { HiMiniUserGroup } from "react-icons/hi2";
 import { FaUserTie, FaUserCheck } from "react-icons/fa6";
-import { MdModeEditOutline } from "react-icons/md";
+import { MdEdit, MdModeEditOutline } from "react-icons/md";
 import { IoMdEye } from "react-icons/io";
 import { usePage } from "@inertiajs/react";
 import Table from "@/Components/Tables/Table";
@@ -11,6 +11,8 @@ import GlobalLink from "@/Components/Atoms/GlobalLink";
 import Notif1 from "@/Components/Notifications/Notif1";
 import { useDeferredValue, useEffect, useRef, useState } from "react";
 import CardCount from "@/Components/Molecules/Cards/CardCount";
+import { IoSearchOutline } from "react-icons/io5";
+import Input from "@/Components/Input/Input";
 
 const Index = () => {
     // Destructure props from usePage()
@@ -32,7 +34,6 @@ const Index = () => {
         setLoading(true);
         const res = await axios.get(route("seksi.complaints-index"), {
             params: {
-                seksisName: auth.user.full_name,
                 keyword: deferredSearch,
             },
         });
@@ -62,6 +63,8 @@ const Index = () => {
     //     //   listener.stopListening(); // Unbind the event listener when the component unmounts
     //     // };
     // }, [echo]);
+
+    console.log("ini allAccountWorkerDatas: ", allAccountWorkerDatas);
 
     return (
         <>
@@ -94,40 +97,26 @@ const Index = () => {
             {/* Main Table Component */}
             <Table>
                 {/* <!-- Search input --> */}
-                <div className="flex lg:justify-end flex-1 lg:mt-5 lg:mr-32 w-full">
-                    <div className="relative w-full max-w-xl mr-6 focus-within:text-purple-500">
-                        <div className="absolute inset-y-0 flex items-center pl-2">
-                            <svg
-                                className="w-4 h-4"
-                                aria-hidden="true"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                    clip-rule="evenodd"
-                                ></path>
-                            </svg>
-                        </div>
-                        <input
-                            className="w-full pl-8 pr-2 text-sm text-gray-700 placeholder-gray-600 bg-gray-100 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-gray dark:focus:placeholder-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:placeholder-gray-500 focus:bg-white focus:border-purple-300 focus:outline-none focus:shadow-outline-purple form-input"
-                            type="text"
-                            placeholder="Search for projects"
-                            aria-label="Search"
-                        />
-                    </div>
+                <div className="flex lg:justify-end lg:mt-5 lg:mr-32 w-full">
+                    <Input className={"basis-2/5"}>
+                        <Input.Label>
+                            <Input.InputText
+                                maxWidth="xl"
+                                placeholder="Search Pengaduan ..."
+                                onChange={(e) => setKeyword(e.target.value)}
+                                leftIcon={<IoSearchOutline />}
+                            />
+                        </Input.Label>
+                    </Input>
                 </div>
 
                 {/* Main Table Component */}
                 <Table.Main className="mt-5">
                     <Table.TableHead>
-                        <Table.Th>Nama Pemohon</Table.Th>
+                        <Table.Th>User Info</Table.Th>
                         <Table.Th>status</Table.Th>
-                        <Table.Th>media</Table.Th>
-                        <Table.Th>kecamatan</Table.Th>
-                        <Table.Th>desa</Table.Th>
-                        <Table.Th>Tindakan</Table.Th>
+                        <Table.Th>role</Table.Th>
+                        <Table.Th>action</Table.Th>
                     </Table.TableHead>
                     <Table.TableBody>
                         {loading === false ? (
@@ -135,60 +124,63 @@ const Index = () => {
                                 searchResults.data.length > 0 ? (
                                     searchResults.data.map(
                                         (
-                                            { complaint, complaint_status, id },
+                                            {
+                                                full_name,
+                                                email,
+                                                avatar,
+                                                status,
+                                                roles,
+                                                id,
+                                            },
                                             index
                                         ) => (
                                             <Table.Tr key={index}>
                                                 <Table.TdProfile
-                                                    name={
-                                                        complaint.user.full_name
-                                                    }
-                                                    email={complaint.user.email}
-                                                    src={complaint.user.avatar}
+                                                    name={full_name}
+                                                    email={email}
+                                                    src={avatar}
                                                 />
                                                 <Table.TdStatus
-                                                    status={
-                                                        complaint_status.name
+                                                    status={status}
+                                                    theme={
+                                                        status === "online"
+                                                            ? "success"
+                                                            : "warning"
                                                     }
-                                                    description={
-                                                        complaint_status.description
-                                                    }
-                                                    theme={complaintStatus(
-                                                        complaint_status.slug
-                                                    )}
                                                 />
                                                 <Table.TdBasic>
                                                     <div
                                                         className="tooltip tooltip-left"
-                                                        data-tip={
-                                                            complaint
-                                                                .complaint_media_type
-                                                                .description
-                                                        }
+                                                        data-tip={"ok"}
                                                     >
-                                                        {
-                                                            complaint
-                                                                .complaint_media_type
-                                                                .name
-                                                        }
+                                                        {roles[0].name}
                                                     </div>
                                                 </Table.TdBasic>
-                                                <Table.TdBasic>
-                                                    {complaint.subdistrict.name}
-                                                </Table.TdBasic>
-                                                <Table.TdBasic>
-                                                    {complaint.village.name}
-                                                </Table.TdBasic>
-                                                <Table.TdBasic>
+                                                <Table.TdBasic
+                                                    className={"flex gap-2"}
+                                                >
                                                     <GlobalLink
                                                         href={route(
-                                                            "complaint-handling.edit",
+                                                            "super-admin.dashboard-manages-worker-accounts-show",
+                                                            id
+                                                        )}
+                                                        children={
+                                                            <IoMdEye className="w-5 h-5 group-hover:text-white transition duration-300 ease-in-out" />
+                                                        }
+                                                        theme="daisyui-info"
+                                                        maxWidth="max"
+                                                        className="btn-outline group"
+                                                    />
+                                                    <GlobalLink
+                                                        href={route(
+                                                            "super-admin.dashboard-manages-worker-accounts-edit-password",
                                                             id
                                                         )}
                                                         children={
                                                             <MdEdit className="w-5 h-5 group-hover:text-white transition duration-300 ease-in-out" />
                                                         }
                                                         theme="daisyui-waring"
+                                                        maxWidth="max"
                                                         className="btn-outline group"
                                                     />
                                                 </Table.TdBasic>
@@ -207,56 +199,63 @@ const Index = () => {
                             ) : allAccountWorkerDatas.data.length > 0 ? (
                                 allAccountWorkerDatas.data.map(
                                     (
-                                        { complaint, complaint_status, id },
+                                        {
+                                            full_name,
+                                            email,
+                                            avatar,
+                                            status,
+                                            roles,
+                                            id,
+                                        },
                                         index
                                     ) => (
                                         <Table.Tr key={index}>
                                             <Table.TdProfile
-                                                name={complaint.user.full_name}
-                                                email={complaint.user.email}
-                                                src={complaint.user.avatar}
+                                                name={full_name}
+                                                email={email}
+                                                src={avatar}
                                             />
                                             <Table.TdStatus
-                                                status={complaint_status.name}
-                                                description={
-                                                    complaint_status.description
+                                                status={status}
+                                                theme={
+                                                    status === "online"
+                                                        ? "success"
+                                                        : "warning"
                                                 }
-                                                theme={complaintStatus(
-                                                    complaint_status.slug
-                                                )}
                                             />
                                             <Table.TdBasic>
                                                 <div
                                                     className="tooltip tooltip-left"
-                                                    data-tip={
-                                                        complaint
-                                                            .complaint_media_type
-                                                            .description
-                                                    }
+                                                    data-tip={"ok"}
                                                 >
-                                                    {
-                                                        complaint
-                                                            .complaint_media_type
-                                                            .name
-                                                    }
+                                                    {roles[0].name}
                                                 </div>
                                             </Table.TdBasic>
-                                            <Table.TdBasic>
-                                                {complaint.subdistrict.name}
-                                            </Table.TdBasic>
-                                            <Table.TdBasic>
-                                                {complaint.village.name}
-                                            </Table.TdBasic>
-                                            <Table.TdBasic>
+                                            <Table.TdBasic
+                                                className={"flex gap-2"}
+                                            >
                                                 <GlobalLink
                                                     href={route(
-                                                        "complaint-handling.edit",
+                                                        "super-admin.dashboard-manages-worker-accounts-show",
+                                                        id
+                                                    )}
+                                                    children={
+                                                        <IoMdEye className="w-5 h-5 group-hover:text-white transition duration-300 ease-in-out" />
+                                                    }
+                                                    theme="daisyui-info"
+                                                    maxWidth="max"
+                                                    className="btn-outline group"
+                                                />
+                                                <GlobalLink
+                                                    href={route(
+                                                        "super-admin.dashboard-manages-worker-accounts-edit-password",
                                                         id
                                                     )}
                                                     children={
                                                         <MdEdit className="w-5 h-5 group-hover:text-white transition duration-300 ease-in-out" />
                                                     }
                                                     theme="daisyui-waring"
+                                                    maxWidth="max"
                                                     className="btn-outline group"
                                                 />
                                             </Table.TdBasic>
@@ -286,12 +285,12 @@ const Index = () => {
                     </Table.TableBody>
                 </Table.Main>
                 <Table.Footer
-                    showFrom={paginationComplaint.from}
-                    showTo={paginationComplaint.to}
-                    total={paginationComplaint.total}
-                    links={paginationComplaint.links}
-                    last_page_url={paginationComplaint.last_page_url}
-                    first_page_url={paginationComplaint.first_page_url}
+                    showFrom={allAccountWorkerDatas.from}
+                    showTo={allAccountWorkerDatas.to}
+                    total={allAccountWorkerDatas.total}
+                    links={allAccountWorkerDatas.links}
+                    last_page_url={allAccountWorkerDatas.last_page_url}
+                    first_page_url={allAccountWorkerDatas.first_page_url}
                 />
             </Table>
         </>
