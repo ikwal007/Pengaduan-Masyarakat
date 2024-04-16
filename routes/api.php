bd3cf424c24ab8e42b8acde4a7094a485cc0a495
Muhammad Ikwal Ramadhan <muhammadikwalr@gmail.com>
2024-04-16 14:35:54 +0700

    feat: Rute API Refactor untuk meningkatkan pengaturan dan keterbacaan
    Komit ini memperkenalkan fitur baru dengan melakukan refaktorisasi rute
    API untuk meningkatkan pengaturan dan keterbacaan. Fitur ini mengelompokkan
    rute-rute terkait di bawah awalan yang sesuai seperti /complaints dan
    /super-admin, sehingga lebih mudah untuk menemukan titik akhir untuk
    fungsi-fungsi tertentu. Pengontrol rute diganti namanya untuk mencerminkan
    tujuannya secara lebih akurat. Komentar yang berlebihan dan pernyataan
    debug dihapus untuk merampingkan basis kode. Nama variabel dan konvensi
    pemformatan distandarisasi untuk konsistensi. Kode dibuat lebih ringkas
    dan lebih mudah dipahami dengan melakukan refactoring pada segmen kode
    yang berlebihan atau bertele-tele.

--- routes/api.php
+++ routes/api.php
@@ -24,10 +24,25 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/complaints', [GetComplaintController::class, 'index'])->name('masyarakat.complaints-index');
-Route::get('/pelayanan/search-complaints', [PelayananGetComplaintController::class, 'index'])->name('pelayanan.complaints-index');
-Route::get('/seksi/search-complaints', [SeksiGetComplaintController::class, 'index'])->name('seksi.complaints-index');
-Route::get('/super-admin/search-worker-accounts', [GetWorkerAccountsController::class, 'index'])->name('super-admin.worker-accounts-index');
-Route::get('/super-admin/get-all-worker-accounts', [GetAllWorkerAccountsController::class, 'index'])->name('super-admin.get-all-worker-accounts-index');
-Route::get('/general/get-notification', [GetNotificationController::class, 'index'])->name('general.get-notification-index');
-Route::get('/general/get-notification/{id}/{email}', [GetNotificationController::class, 'update'])->name('general.get-notification-update');
+// Complaints routes
+Route::prefix('complaints')->group(function () {
+    Route::get('/', [GetComplaintController::class, 'index'])->name('masyarakat.complaints-index');
+    Route::prefix('pelayanan')->group(function () {
+        Route::get('/search', [PelayananGetComplaintController::class, 'index'])->name('pelayanan.complaints-index');
+    });
+    Route::prefix('seksi')->group(function () {
+        Route::get('/search', [SeksiGetComplaintController::class, 'index'])->name('seksi.complaints-index');
+    });
+});
+
+// Super Admin routes
+Route::prefix('super-admin')->group(function () {
+    Route::get('/worker-accounts', [GetWorkerAccountsController::class, 'index'])->name('super-admin.worker-accounts-index');
+    Route::get('/all-worker-accounts', [GetAllWorkerAccountsController::class, 'index'])->name('super-admin.get-all-worker-accounts-index');
+});
+
+// General routes
+Route::prefix('general')->group(function () {
+    Route::get('/notification', [GetNotificationController::class, 'index'])->name('general.get-notification-index');
+    Route::get('/notification/{id}/{email}', [GetNotificationController::class, 'update'])->name('general.get-notification-update');
+});
