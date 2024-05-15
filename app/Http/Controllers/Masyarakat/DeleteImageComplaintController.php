4a3b8e0c4b8bf7dc527e772e410f4c0bd99365ea
Muhammad Ikwal Ramadhan <muhammadikwalr@gmail.com>
2024-05-15 10:07:24 +0700

    Feat(Masyarakat Controllers): Add Controller to DeleteImageComplaint

---
+++ app/Http/Controllers/Masyarakat/DeleteImageComplaintController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Masyarakat;
+
+use App\Http\Controllers\Controller;
+use App\Models\Archives;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class DeleteImageComplaintController extends Controller
+{
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $findArchive = Archives::find($id);
+        Storage::delete($findArchive->resource);
+        $findArchive->delete();
+        return response()->json(['message' => 'success'], 200);
+    }
+}
